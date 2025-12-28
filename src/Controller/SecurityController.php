aadc1390bd32c2ed9c533d8b4b4ada277c83e292
Vladimir Leitenberger <vladimir.leitenberger@gmail.com>
2025-12-28 16:37:27 +0100

    add basic auth and login-forms (classic + passphrase)

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'pico_admin_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('_login.html.twig', [
+            'last_username' => 'pico_admin', // in-memory admin
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'pico_admin_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
